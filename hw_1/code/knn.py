6c34100312089d3177de21435563855548fb6339
Asklepiad <bogdan.sotnikov.1999@mail.ru>
2023-02-12 21:14:01 +0600

    Added jn, 2 python modules and requirements

---
+++ hw_1/code/knn.py
@@ -0,0 +1,146 @@
+import numpy as np
+
+
+class KNNClassifier:
+    """
+    K-neariest-neighbor classifier using L1 loss
+    """
+    
+    def __init__(self, k=1):
+        self.k = k
+    
+
+    def fit(self, X, y):
+        self.train_X = X
+        self.train_y = y
+
+
+    def predict(self, X, n_loops=0):
+        """
+        Uses the KNN model to predict clases for the data samples provided
+        
+        Arguments:
+        X, np array (num_samples, num_features) - samples to run
+           through the model
+        num_loops, int - which implementation to use
+
+        Returns:
+        predictions, np array of ints (num_samples) - predicted class
+           for each sample
+        """
+        
+        if n_loops == 0:
+            distances = self.compute_distances_no_loops(X)
+        elif n_loops == 1:
+            distances = self.compute_distances_one_loop(X)
+        else:
+            distances = self.compute_distances_two_loops(X)
+        
+        if len(np.unique(self.train_y)) == 2:
+            return self.predict_labels_binary(distances)
+        else:
+            return self.predict_labels_multiclass(distances)
+
+
+    def compute_distances_two_loops(self, X):
+        """
+        Computes L1 distance from every sample of X to every training sample
+num_test = X
+        num_train = self.train_X        Uses simplest implementation with 2 Python loops
+
+        Arguments:
+        X, np array (num_test_samples, num_features) - samples to run
+        
+        Returns:
+        distances, np array (num_test_samples, num_train_samples) - array
+           with distances between each test and each train sample
+        """
+    
+        num_test = X
+        num_train = self.train_X
+        distances = np.empty((len(num_test), len(num_train)), "float64")
+        for i in range(len(num_test)):
+            for j in range(len(num_train)):
+                distances[i][j] = np.sum(np.abs(num_test[i]-num_train[j]))
+        return distances         
+       
+
+
+    def compute_distances_one_loop(self, X):
+        """
+        Computes L1 distance from every sample of X to every training sample
+        Vectorizes some of the calculations, so only 1 loop is used
+
+        Arguments:
+        X, np array (num_test_samples, num_features) - samples to run
+        
+        Returns:
+        distances, np array (num_test_samples, num_train_samples) - array
+           with distances between each test and each train sample
+        """
+
+        num_test = X
+        num_train = self.train_X
+        distances = np.empty((len(num_test), len(num_train)), "float64")
+        for i in range(len(num_test)):
+            distances[i, :] = np.sum(np.abs(num_test[i] - num_train), axis=1)
+        return distances
+
+
+    def compute_distances_no_loops(self, X):
+        """
+        Computes L1 distance from every sample of X to every training sample
+        Fully vectorizes the calculations using numpy
+
+        Arguments:
+        X, np array (num_test_samples, num_features) - samples to run
+        
+        Returns:
+        distances, np array (num_test_samples, num_train_samples) - array
+           with distances between each test and each train sample
+        """
+
+        
+        num_test = X
+        num_train = self.train_X
+        distances = np.abs(num_test[:,None] - num_train).sum(-1)
+        return distances
+        
+
+
+    def predict_labels_binary(self, distances):
+        """
+        Returns model predictions for binary classification case
+        
+        Arguments:
+        distances, np array (num_test_samples, num_train_samples) - array
+           with distances between each test and each train sample
+        Returns:
+        pred, np array of bool (num_test_samples) - binary predictions 
+           for every test sample
+        """
+
+        n_train = distances.shape[1]
+        n_test = distances.shape[0]
+        prediction = np.zeros(n_test)        
+        prediction = self.train_y[np.argpartition(distances, self.k - 1, axis=1)[:,0:self.k]].astype("int64").mean(axis=1).round().astype("bool")
+        return prediction
+        
+        
+    def predict_labels_multiclass(self, distances):
+        """
+        Returns model predictions for multi-class classification case
+        
+        Arguments:
+        distances, np array (num_test_samples, num_train_samples) - array
+           with distances between each test and each train sample
+        Returns:
+        pred, np array of int (num_test_samples) - predicted class index 
+           for every test sample
+        """
+
+        n_train = distances.shape[0]
+        n_test = distances.shape[0]
+        prediction = np.zeros(n_test, "int64")
+        prediction = self.train_y[np.argpartition(distances, self.k - 1, axis=1)[:,0:self.k]].astype("int64").mean(axis=1).round().astype("int16")
+        return prediction
